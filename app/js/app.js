5b6f4be5a35fe8334fd65892b001e550a8cdf86b
georgiamoon <georgia.bullen@gmail.com>
2013-07-30 16:20:37 -0400

    red hook updates for splash page

--- app/js/app.js
+++ app/js/app.js
@@ -7,6 +7,9 @@ var app = angular.module('Tidepools', ['tidepoolsFilters', 'tidepoolsServices','
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/', {templateUrl: 'partials/landmark-list.html', controller: LandmarkListCtrl}). //loading template with controller
+      when('/about', {templateUrl: 'partials/aboutRHIWiFi.html', controller: LandmarkListCtrl}). //loading template with controller
+      when('/contact', {templateUrl: 'partials/contact.html', controller: LandmarkListCtrl}). //loading template with controller
+      when('/feedback', {templateUrl: 'partials/feedback.html', controller: LandmarkListCtrl}). //loading template with controller
       when('/landmark/:landmarkId', {templateUrl: 'partials/landmark-detail.html', controller: LandmarkDetailCtrl}). //loading template + controller
       when('/new', {templateUrl: 'partials/landmark-new.html', controller: LandmarkNewCtrl}). //loading template + controller
       when('/talk', {templateUrl: 'partials/talk-list.html', controller: talklistCtrl}). //loading template + controller
