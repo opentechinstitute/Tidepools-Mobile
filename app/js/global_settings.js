55d924df0ff3d688dff733713bbf3119921544eb
georgiamoon <georgia.bullen@gmail.com>
2013-09-12 23:12:05 -0400

    filters and types for the events
    Seamus: icons now call subtype, but I wasn't sure where those match up
    to icon file names. we can check in in the AM

--- app/js/global_settings.js
+++ app/js/global_settings.js
@@ -41,9 +41,9 @@ var global_hashtag = "#is4cwn";
 //-------------------------------//
 
 
-var eventCategories = ['Meeting','Party','Hangout'];
+var eventCategories = ['IS4CWN-Keynote','IS4CWN-Presentation','IS4CWN-Film','IS4CWN-Information','IS4CWN-Hack','Meet','Collaborate','Eat'];
 
-var placeCategories = ['Organization','Shop','Food/Drink','Park'];
+var placeCategories = ['Session Room','HackLab','Bar/Restaurant','Hotel','Outside','Park','Tourism Spot'];
 
 
 var globalEditLoc = {}; //this is a temp variable for an issue with angular leaflet directive in landmark-edit
